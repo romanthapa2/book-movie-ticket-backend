e4cede2bc523cd03d967a55a66308513221fc5cc
roman thapa <thaparoman970@gmail.com>
2024-07-04 23:13:59 +0545

    update : db connected

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const app=require('./app')
-
+const connectMongoDB=require('./db/index.js')
 
 const port=process.env.PORT;
 
-app.listen(port,()=>[
+connectMongoDB()
+.then(()=>{
+app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
-])
+})
+})
+.catch((error)=>{
+    console.log("mongodb connection failed",error);
+})
+
